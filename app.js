2aaedc0e7b745ebdbb32a6fb83f188751fb321b3
Bart Frackiewicz <decomplosed@gmail.com>
2020-06-21 23:16:46 +0200

    imported express handlebars

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
+const exphbs = require('express-handlebars')
 const connectDB = require('./config/db')
 
 // Load config
