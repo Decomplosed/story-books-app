e0a427d6d15d5d9926e2515b37ea68afc0ca194d
Bart Frackiewicz <decomplosed@gmail.com>
2020-06-21 23:20:02 +0200

    added defaultLayout

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 }
 
-app.engine('.hbs', exphbs({ extname: '.hbs' }))
+app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', '.hbs')
 
 const PORT = process.env.PORT || 3000
