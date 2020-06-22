55ac35f38d710f3de7d028c52e592cb3f38e7545
Bart Frackiewicz <decomplosed@gmail.com>
2020-06-22 15:07:52 +0200

    formatting fix

--- app.js
+++ app.js
@@ -22,23 +22,26 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 // Handlebars
-
 app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', '.hbs')
 
 // Sessions
+app.use(
+  session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+  })
+)
 
 // Passport middleware
-
 app.use(passport.initialize())
 app.use(passport.session())
 
 // Static folder
-
 app.use(express.static(path.join(__dirname, 'public')))
 
 // Routes
-
 app.use('/', require('./routes/index'))
 
 const PORT = process.env.PORT || 3000
