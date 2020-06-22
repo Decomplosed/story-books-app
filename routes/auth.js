179ca738057689b760abf027cc5cc6b71d4ec682
Bart Frackiewicz <decomplosed@gmail.com>
2020-06-22 15:23:00 +0200

    started setting up auth route

--- routes/auth.js
+++ routes/auth.js
@@ -1,3 +1,8 @@
 const express = require('express')
 const router = express.Router()
 const passport = require('passport')
+
+// @desc Auth with Google
+// @route GET /auth/google
+router.get('/google', passport.authenticate('google', {}))
+
