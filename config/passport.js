38758d382edcee730bcf0d14f67c32aa69d8a9d6
Bart Frackiewicz <decomplosed@gmail.com>
2020-06-22 15:16:05 +0200

    added callback async function

--- config/passport.js
+++ config/passport.js
@@ -4,10 +4,15 @@ const User = require('../models/User')
 
 module.exports = function (passport) {
   passport.use(
-    new GoogleStrategy({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: '/auth/google/callback',
-    })
+    new GoogleStrategy(
+      {
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: '/auth/google/callback',
+      },
+      async (accessToken, refreshToken, profile, done) => {
+        console.log(profile)
+      }
+    )
   )
 }
