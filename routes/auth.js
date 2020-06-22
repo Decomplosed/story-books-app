8bbd99e4fd0a99f81457e4bf85e1a8cde9f236fc
Bart Frackiewicz <decomplosed@gmail.com>
2020-06-22 15:23:30 +0200

    added scope of profile

--- routes/auth.js
+++ routes/auth.js
@@ -4,5 +4,4 @@ const passport = require('passport')
 
 // @desc Auth with Google
 // @route GET /auth/google
-router.get('/google', passport.authenticate('google', {}))
-
+router.get('/google', passport.authenticate('google', { scope: ['profile'] }))
