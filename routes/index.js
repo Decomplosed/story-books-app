3eb5d33346648fc3db2badee218c1082dc101106
Bart Frackiewicz <decomplosed@gmail.com>
2020-06-23 16:17:30 +0200

    fixed typo inside import

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const { ensureAuth, ensureGuest } = require('../middleware/auths')
+const { ensureAuth, ensureGuest } = require('../middleware/auth')
 
 // @desc Login/Landing page
 router.get('/', ensureGuest, (req, res) => {
