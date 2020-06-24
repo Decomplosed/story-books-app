027aa72c7cb038eb94b1fabf0a7eee496e28c4fd
Bart Frackiewicz <decomplosed@gmail.com>
2020-06-24 13:44:14 +0200

    created a route for adding a story

--- routes/stories.js
+++ routes/stories.js
@@ -4,4 +4,10 @@ const { ensureAuth } = require('../middleware/auth')
 
 const Story = require('../models/Story')
 
+// @desc  Show add page
+// @route GET /stories/add
+router.get('/add', ensureAuth, (req, res) => {
+  res.render('stories/add')
+})
+
 module.exports = router
