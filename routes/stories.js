ac28832faf1a88078dbf7faedfbb41b1657cc530
Bart Frackiewicz <decomplosed@gmail.com>
2020-06-26 19:21:41 +0200

    if theres any error with story return 404 page

--- routes/stories.js
+++ routes/stories.js
@@ -63,6 +63,10 @@ router.get('/edit/:id', ensureAuth, async (req, res) => {
 // @route PUT /stories/:id
 router.put('/:id', ensureAuth, async (req, res) => {
   let story = await Story.findById(req.params.id).lean()
+
+  if (!story) {
+    return res.render('error/404')
+  }
 })
 
 module.exports = router
