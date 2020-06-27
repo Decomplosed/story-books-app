c6e05cfab49cff337649544d82daa5259704739e
Bart Frackiewicz <decomplosed@gmail.com>
2020-06-27 22:33:45 +0200

    redirect to dashboard

--- routes/stories.js
+++ routes/stories.js
@@ -85,6 +85,7 @@ router.put('/:id', ensureAuth, async (req, res) => {
 router.delete('/:id', ensureAuth, async (req, res) => {
   try {
     await Story.remove({ _id: req.params.id })
+    res.redirect('/dashboard')
   } catch (err) {}
 })
 
